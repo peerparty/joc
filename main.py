7f2b1c35871098077f885ba9f0def8721937625c
JBG <jbg@hacker.coffee>
2019-05-26 16:48:51 +0200

    omg it kind of works

--- main.py
+++ main.py
@@ -6,10 +6,11 @@ import time
 import threading
 
 from functools import reduce
-from anytree import Node, RenderTree, LevelOrderGroupIter, LevelOrderIter
+from anytree import Node, RenderTree, LevelOrderGroupIter, LevelOrderIter, PostOrderIter
+from enum import Enum
 
 round_time = 2 
-user_time = 1
+user_time = 1 
 
 class User:
     def __init__(self, user_id):
@@ -27,12 +28,17 @@ class Answer:
         self.user = user
         self.val = val
 
+class QuestionState(Enum):
+    OPEN = 1 
+    CLOSED = 2 
+    REASKED = 3 
+    PROMPTED = 4 
+
 class Question(Node):
     def __init__(self, *args, **kwargs):
         super(self.__class__, self).__init__(*args, **kwargs)
         self.answers = []
-        self.closed = False
-        self.reasked = False 
+        self.state = QuestionState.OPEN
 
     def add_answer(self, ans):
         self.answers.append(ans)
@@ -57,13 +63,26 @@ class Question(Node):
                 count_no += 1
         return (count_yes > 0 and count_no == 0) or (count_no > 0 and count_yes == 0)
 
+#test_ans = [True, False, True, False, False, False, False, False, False, False]
+test_ans = [True, False, True, False, True, True, False, False, False, True, False, True, False, True, False, True, False, False, True, True, True, False, True, True, True, True, True, True, False, True, False, True, False, False, True, False, True, True, True, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False]
+
+test_count = 0
+test_input = []
+
 def answer_question(user):
+    global test_count
+    global test_ans
+    global test_input
     if len(user.questions) > 0:
         q = user.questions.pop(0)
         time.sleep(random.randint(1, user_time))
+        #q.add_answer(Answer(user, random.randint(0,1) == 1))
+        #q.add_answer(Answer(user, False if test_count >= len(test_ans) else test_ans[test_count]))
+        ans = False if ques_count > 100 else random.randint(0,1) == 1
+        q.add_answer(Answer(user, ans))
+        test_input.append(ans)
+        test_count += 1
         print("user %d answers %s" % (user.id, q.name))
-        print_root()
-        q.add_answer(Answer(user, True if get_height() > 3 else random.randint(0,1) == 1))
 
 def create_question(user):
     if len(user.prompts) > 0:
@@ -90,44 +109,54 @@ def get_height():
     return len([[node for node in children] for children in LevelOrderGroupIter(root)])
 
 def get_siblings(question):
-    questions = [[node for node in children] for children in LevelOrderGroupIter(root)]
-    for level in questions:
-        if question in level:
-            return level 
+    return question.parent.children
+    #questions = [[node for node in children] for children in LevelOrderGroupIter(root)]
+    #for level in questions:
+    #    if question in level:
+    #        return level 
 
 def sibling_consensus(question):
     sibs = get_siblings(question)
+    sibs = list(filter(lambda q : not q.state == QuestionState.REASKED, sibs))
     print("SIBS %s" % [node.name for node in sibs])
-    if len(sibs) > 1 and reduce(lambda a, q : not q.reasked and q.check_consensus() and a, sibs, True):
+    if len(sibs) > 1 and reduce(lambda a, q : q.check_consensus() and a, sibs, True):
         parent = question.parent
-        if not parent.closed:
-            print("the reask %s" % parent.name)
-            input()
-            parent.closed = True
+        if not parent.state == QuestionState.REASKED:
+            print("the reask %s %s" % (parent.name, parent.state))
+            parent.state = QuestionState.REASKED 
             parent.parent.add_question(parent.name)
-            print_root()
 
 def create_prompt(question, ans):
     ans.user.prompts.append(Prompt(question, ans))
 
 def close_answers():
-    print("close_answers")
-    #open_answers(False)
-    #questions = [[node for node in children] for children in LevelOrderGroupIter(root)][-1]
-    questions = list(filter(lambda n : len(n.children) == 0, [node for node in LevelOrderIter(root)]))
-    for question in questions:
-        if not question.closed and not question.check_consensus():
-            print("no consensus")
-            print_root()
-            for ans in question.answers:
-                create_prompt(question, ans)
+    q_size = reduce(lambda c, user : c + len(user.questions), users, 0)
+    if(q_size > 0):
+        collect_answers()
+    else:
+        questions = list(filter(lambda n : n.state == QuestionState.OPEN, [node for node in PostOrderIter(root)]))
+        for question in questions:
+            if not question.check_consensus():
+                print("no consensus %s" % question.name)
+                question.state = QuestionState.PROMPTED 
+                for ans in question.answers:
+                    create_prompt(question, ans)
+            else:
+                print("omg consensus %s" % question.name)
+                question.state = QuestionState.CLOSED 
+                sibling_consensus(question)
+        print_root()
+        print_users()
+
+
+        q_size = reduce(lambda c, user : c + len(user.prompts) + len(user.questions), users, 0)
+        if(q_size > 0):
+            collect_prompts()
         else:
-            print("omg consensus")
-            question.closed = True
+            print("DONE!!!")
             print_root()
-            sibling_consensus(question)
-    print_users()
-    collect_prompts()
+            global test_input
+            print(test_input)
 
 def collect_answers():
     print("collect_answers")
@@ -165,7 +194,7 @@ root = Question("Root")
 def print_root():
     for pre, fill, node in RenderTree(root):
         ans_str = ','.join(map(str, [(ans.user.id, ans.val) for ans in node.answers]))
-        print("%s%s %s" % (pre, node.name, ans_str))
+        print("%s%s,%s %s" % (pre, node.name, node.state, ans_str))
 
 def print_users():
     for user in users:
