44085562ee80a1a02693a41dd71ea0806fbe4323
Your Name <jbg@p2ee.org>
2019-10-11 09:48:03 +0000

    support for different content types

--- hs.py
+++ hs.py
@@ -13,20 +13,27 @@ class HTTP():
    
 class S(BaseHTTPRequestHandler):
 
-  def _set_audio_headers(self):
+  def _get_content_type(self):
+    if 'mpeg' in self.path:
+      return 'audio/mpeg'
+    elif 'gif' in self.path:
+      return 'image/gif'
+    elif 'png' in self.path:
+      return 'image/png'
+    elif 'css' in self.path:
+      return 'text/css'
+    else:
+      return 'text/html'
+
+  def _set_headers(self, content_type):
     self.send_response(200)
-    self.send_header('Content-type', 'audio/mpeg')
+    self.send_header('Content-type', content_type)
     self.end_headers()
 
-  def _set_headers(self):
-    self.send_response(200)
-    self.send_header('Content-type', 'text/html')
-    self.end_headers()
-
-  def _handle_audio(self):
-    self._set_audio_headers()
-    f = open('./' + self.path, 'rb')
+  def _handle_binary(self):
+    self._set_headers(self._get_content_type())
     try:
+      f = open('./' + self.path, 'rb')
       self.wfile.write(f.read())
       f.close()
       return
@@ -34,9 +41,9 @@ class S(BaseHTTPRequestHandler):
       self.send_error(404, 'file not found')
 
   def _handle_txt(self):
-    self._set_headers()
-    f = open('./' + self.path)
+    self._set_headers(self._get_content_type())
     try:
+      f = open('./' + self.path)
       self.wfile.write(f.read().encode('utf-8'))
       f.close()
       return
@@ -44,13 +51,17 @@ class S(BaseHTTPRequestHandler):
       self.send_error(404, 'file not found')
 
   def do_GET(self):
-    if 'mpeg' in self.path:
-      self._handle_audio()
+    if ('mpeg' in self.path or
+        'gif' in self.path or
+        'png' in self.path or
+        'ico' in self.path):
+      self._handle_binary()
     else:
       if self.path == '/':
         self.path = '/index.html'
-      self._handle_txt()
-      
+      elif self.path == '/admin':
+        self.path = '/admin.html'
+      self._handle_txt()     
 #  def do_POST(self):
 #    self._set_headers()
 #
